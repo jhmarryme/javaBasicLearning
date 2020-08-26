52bcd898cc7a0290652cc147dda19518e6b943ad
wjh <wjh@travelsky.com>
2020-08-26 14:51:19 +0800

    EffectiveJava3rd 学习

--- comprehensivePractice/src/main/java/effectiveJava/_10_equalswithrules/CaseInsensitiveString.java
+++ comprehensivePractice/src/main/java/effectiveJava/_10_equalswithrules/CaseInsensitiveString.java
@@ -4,7 +4,7 @@ import java.util.Objects;
 
 /**
  * description: 10. 重写 equals 方法时遵守通用约定
- *
+ *  对称性验证
  * @Author: Wjh
  * @Date: 2020/8/26 11:24
  * @Modified By:
@@ -17,7 +17,11 @@ public final class CaseInsensitiveString {
         this.s = Objects.requireNonNull(s);
     }
 
-    // Broken - violates symmetry!
+    /*
+     * description: 不具备对称性的equals方法, a.equals(b) = true, b.equals(a) = false;
+     * @Author: Wjh
+     * @Date: 2020/8/26 14:14
+     */
     @Override
     public boolean equals(Object o) {
         if (o instanceof CaseInsensitiveString)
@@ -27,4 +31,16 @@ public final class CaseInsensitiveString {
             return s.equalsIgnoreCase((String) o);
         return false;
     }
+
+    /*
+     * Description: 具备对称性的equals方法
+     * @Author: Wjh
+     * @Date: 2020/8/26 14:16
+     */
+    /*@Override
+    public boolean equals(Object o) {
+        return o instanceof CaseInsensitiveString &&
+                ((CaseInsensitiveString) o).s.equalsIgnoreCase(s);
+    }*/
+
 }
