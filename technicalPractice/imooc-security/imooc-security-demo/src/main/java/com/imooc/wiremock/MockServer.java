fa1306398730462b0a10345a3f810776e83802f9
wjh <wjh@travelsky.com>
2020-09-11 17:27:55 +0800

    mockserver

---
+++ technicalPractice/imooc-security/imooc-security-demo/src/main/java/com/imooc/wiremock/MockServer.java
@@ -0,0 +1,30 @@
+package com.imooc.wiremock;
+
+import com.github.tomakehurst.wiremock.client.WireMock;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.IOException;
+
+public class MockServer {
+
+    /**
+     * @param args
+     * @throws IOException
+     */
+    public static void main(String[] args) throws IOException {
+        WireMock.configureFor(8062);
+        WireMock.removeAllMappings();
+
+        mock("/order/1", "01");
+        mock("/order/2", "02");
+    }
+
+    private static void mock(String url, String file) throws IOException {
+        ClassPathResource resource = new ClassPathResource("mock/response/" + file + ".txt");
+        String content = StringUtils.join(FileUtils.readLines(resource.getFile(), "UTF-8").toArray(), "\n");
+        WireMock.stubFor(WireMock.get(WireMock.urlPathEqualTo(url)).willReturn(WireMock.aResponse().withBody(content).withStatus(200)));
+    }
+
+}
