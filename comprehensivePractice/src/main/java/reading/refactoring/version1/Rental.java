1b015c17260908ef8e9a405d486de5c0a47ae723
wjh <wjh@travelsky.com>
2020-10-12 09:08:36 +0800

    重构学习 - version1

---
+++ comprehensivePractice/src/main/java/reading/refactoring/version1/Rental.java
@@ -0,0 +1,27 @@
+package reading.refactoring.version1;
+
+/**
+ * description: 
+ * @Author: Wjh
+ * @Date: 2020/10/12 8:47
+ * @Modified By:
+ */
+public class Rental {
+    // 影片
+    private Movie _movie;
+    // 租期
+    private int _daysRented;
+
+    public Rental(Movie movie, int daysRented) {
+        _movie = movie;
+        _daysRented = daysRented;
+    }
+
+    public int getDaysRented() {
+        return _daysRented;
+    }
+
+    public Movie getMovie() {
+        return _movie;
+    }
+}
