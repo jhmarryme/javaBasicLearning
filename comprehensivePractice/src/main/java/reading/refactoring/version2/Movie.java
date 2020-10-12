db5355e66cb2801d28821de1f131a5815f6d71fe
wjh <wjh@travelsky.com>
2020-10-12 10:17:29 +0800

    重构学习 - version2

---
+++ comprehensivePractice/src/main/java/reading/refactoring/version2/Movie.java
@@ -0,0 +1,33 @@
+package reading.refactoring.version2;
+
+/**
+ * description: 
+ * @Author: Wjh
+ * @Date: 2020/10/12 8:40
+ * @Modified By:
+ */
+public class Movie {
+
+    public static final int CHILDRENS = 2;
+    public static final int REGULAR = 0;
+    public static final int NEW_RELEASE = 1;
+    private String _title;
+    private int _priceCode;
+
+    public Movie(String title, int priceCode) {
+        _title = title;
+        _priceCode = priceCode;
+    }
+
+    public int getPriceCode() {
+        return _priceCode;
+    }
+
+    public String getTitle() {
+        return _title;
+    }
+
+    public void setPriceCode(int priceCode) {
+        _priceCode = priceCode;
+    }
+}
