abcdd1003923dcf9e2bc8f70cd4c26f668573443
wjh <wjh@travelsky.com>
2020-10-14 18:15:53 +0800

    guava学习

--- comprehensivePractice/src/main/java/reading/guava/baseUtil/PreconditionsTest.java
+++ comprehensivePractice/src/main/java/reading/guava/baseUtil/PreconditionsTest.java
@@ -6,8 +6,12 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import utils.CreateUtils;
 
+import java.util.Date;
+
 /**
- * description: 
+ * description: 前置条件
+ *
+ *  https://wizardforcel.gitbooks.io/guava-tutorial/content/4.html
  * @Author: Wjh
  * @Date: 2020/10/13 10:15
  * @Modified By:
@@ -16,15 +20,24 @@ public class PreconditionsTest {
 
     @Test
     public void preconditionsTest() {
-        boolean received = receiveParameters(CreateUtils.createStudent(true));
+        // 检测参数
+        boolean received = receiveParameters(CreateUtils.createStudent(false));
         Assertions.assertTrue(received);
 
+        Assertions.assertFalse(receiveParameters(null));
+
+        System.out.println(new Date().toString());
     }
 
 
     public static boolean receiveParameters(Student student) {
-        // failed -> NullPointerException
-        Student stu = Preconditions.checkNotNull(student);
+        try {
+            // failed -> NullPointerException
+            Student stu = Preconditions.checkNotNull(student);
+        } catch (Exception e) {
+            return false;
+        }
+        System.out.println(student.toString());
         return true;
     }
 }
