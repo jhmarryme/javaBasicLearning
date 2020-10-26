60a7c17b24ce50c487c0cc4e9d72d64501e4da74
wjh <wjh@travelsky.com>
2020-10-26 12:58:13 +0800

    调整目录结构

---
+++ comprehensivePractice/src/main/java/reading/coreJava/example/corejava/v2ch11/serialTransfer/SerialTransferTest.java
@@ -0,0 +1,23 @@
+package coreJava.example.corejava.v2ch11.serialTransfer;
+
+import java.awt.*;
+import javax.swing.*;
+
+/**
+ * This program demonstrates the transfer of serialized objects between virtual machines.
+ * @version 1.04 2018-05-01
+ * @author Cay Horstmann
+ */
+public class SerialTransferTest
+{
+   public static void main(String[] args)
+   {
+      EventQueue.invokeLater(() ->
+         {
+            var frame = new SerialTransferFrame();
+            frame.setTitle("SerialTransferTest");
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setVisible(true);
+         });
+   }
+}
---
+++ comprehensivePractice/src/main/java/reading/imooc/javaBasic/reflect/Robot.java
@@ -0,0 +1,19 @@
+package reading.imooc.javaBasic.reflect;
+
+/**
+ * @author jhmarryme.cn
+ * @date 2019/7/24 19:15
+ */
+public class Robot {
+    private String name;
+    public void sayHi(String helloSentence){
+        System.out.println(helloSentence + " " + name);
+    }
+    private String throwHello(String tag){
+        return "Hello " + tag;
+    }
+    static {
+        System.out.println("Hello Robot");
+    }
+
+}
