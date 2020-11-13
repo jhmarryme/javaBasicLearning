f6913301b3a809fb3db00daf0392888fb010d020
wjh <wjh@travelsky.com>
2020-11-13 16:36:41 +0800

    adapter模式学习

---
+++ comprehensivePractice/src/main/java/reading/designPatternLesson/adapter/adaptee/Movable.java
@@ -0,0 +1,20 @@
+package reading.designPatternLesson.adapter.adaptee;
+
+/**
+ * description: 
+ * @Author: Wjh
+ * @Date: 2020/11/12 16:32
+ * @Modified By:
+ */
+public interface Movable {
+    
+    /**
+     * 
+     * <br/>
+     * @author Jiahao Wang
+     * @date 2020/11/12 17:34
+     * @param a 
+     * @return double
+     */
+    public double getSpeed(int a) throws Exception;
+}
---
+++ comprehensivePractice/src/main/java/reading/designPatternLesson/adapter/client/Client.java
@@ -0,0 +1,10 @@
+package reading.designPatternLesson.adapter.client;
+
+/**
+ * description: 
+ * @Author: Wjh
+ * @Date: 2020/11/12 16:30
+ * @Modified By:
+ */
+public class Client {
+}
