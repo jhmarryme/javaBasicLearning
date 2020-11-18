4565b0ea56491c0419eeeb0d0b87b8af9423db40
wjh <wjh@travelsky.com>
2020-11-18 16:45:32 +0800

    码出高效: 元素的比较学习, hashCode和equals

--- comprehensivePractice/src/main/java/reading/easycoding/datetype/collections/elementcomparison/HashCodeAndEqualsTest.java
+++ comprehensivePractice/src/main/java/reading/easycoding/datetype/collections/elementcomparison/HashCodeAndEqualsTest.java
@@ -6,14 +6,13 @@ import lombok.NoArgsConstructor;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.HashSet;
-import java.util.Objects;
-import java.util.Set;
+import java.util.*;
 
 /**
  * description: hashCode和equals
  *      任何时候重写equals,都必须重写hashCode
- *
+ *      尽量避免通过实例对象引用来调用equals方法,容易NPE, 推荐使用Objects的equals方法
+ *      集合比较时, 具体逻辑需要具体分析
  * @Author: Wjh
  * @Date: 2020/11/18 15:52
  * @Modified By:
@@ -30,6 +29,7 @@ public class HashCodeAndEqualsTest {
         set.add(new EqualsObject(1, "one"));
         set.add(new EqualsObject(1, "one"));
 
+        // 没有重写hashCode时, set不能正确识别重复对象, size = 3
         System.out.println("set.size() = " + set.size());
 
     }
@@ -42,9 +42,22 @@ public class HashCodeAndEqualsTest {
         set.add(new EqualsAndHashCodeObject(1, "one"));
         set.add(new EqualsAndHashCodeObject(1, "one"));
 
+        // 重写hashCode后, set能正确识别重复对象, size = 1
         System.out.println("set.size() = " + set.size());
     }
 
+    @Test
+    @DisplayName("两个相同数据 不同类型的集合类的比较")
+    public void listEquals() {
+        LinkedList<Integer> linkedList = new LinkedList<>();
+        linkedList.add(1);
+
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        arrayList.add(1);
+
+        // equals的实现方式与类的具体实现逻辑有关, 这里会为true
+        System.out.println("arrayList.equals(linkedList) = " + arrayList.equals(linkedList));
+    }
 
     @Data
     @AllArgsConstructor
