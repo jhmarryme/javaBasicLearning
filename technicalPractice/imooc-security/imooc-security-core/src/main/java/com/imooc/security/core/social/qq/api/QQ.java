731cef35c1fb2bd44c7682989c46b30cd1ad2d01
wjh <wjh@travelsky.com>
2021-01-08 16:55:41 +0800

    spring social 学习

---
+++ technicalPractice/imooc-security/imooc-security-core/src/main/java/com/imooc/security/core/social/qq/api/QQ.java
@@ -0,0 +1,13 @@
+package com.imooc.security.core.social.qq.api;
+
+import java.io.IOException;
+
+/**
+ * description: 
+ * @author JiaHao Wang
+ * @date 2021/1/8 12:32
+ */
+public interface QQ {
+
+    QQUserInfo getQQUserInfo() throws Exception;
+}
---
+++ technicalPractice/imooc-security/imooc-security-core/src/main/java/com/imooc/security/core/social/qq/api/QQUserInfo.java
@@ -0,0 +1,9 @@
+package com.imooc.security.core.social.qq.api;
+
+/**
+ * description: 
+ * @author JiaHao Wang
+ * @date 2021/1/8 12:32
+ */
+public class QQUserInfo {
+}
