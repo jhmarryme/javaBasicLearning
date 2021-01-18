d40dd29fa66b299764312fb846179871208d9610
wjh <wjh@travelsky.com>
2021-01-18 17:28:37 +0800

    spring social qq登录 配置 注释添加

--- technicalPractice/imooc-security/imooc-security-core/src/main/java/com/imooc/security/core/social/qq/connect/QQAutoConfig.java
+++ technicalPractice/imooc-security/imooc-security-core/src/main/java/com/imooc/security/core/social/qq/connect/QQAutoConfig.java
@@ -5,14 +5,13 @@ import com.imooc.security.core.properties.SecurityProperties;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.social.SocialAutoConfigurerAdapter;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.social.connect.ConnectionFactory;
-import org.springframework.social.security.SpringSocialConfigurer;
 
 /**
- * description: qq相关配置,
- *      在{@link com.imooc.security.browser.BrowserSecurityConfig} 中被引用
+ * description: qq相关配置, 
+ *      实现 {@link SocialAutoConfigurerAdapter}中createConnectionFactory方法
+ *      当配置文件中存在imooc.security.social.qq.appId的时候, 该配置才会生效
  * @author JiaHao Wang
  * @date 2021/1/13 15:15
  */
@@ -25,9 +24,8 @@ public class QQAutoConfig extends SocialAutoConfigurerAdapter {
 
     @Override
     protected ConnectionFactory<?> createConnectionFactory() {
-
         QQProperties qqProperties = securityProperties.getSocial().getQq();
-
+        // 提供 ProviderId, appId, AppSecret
         return new QQConnectionFactory(qqProperties.getPrividerId(), qqProperties.getAppId(), qqProperties.getAppSecret());
     }
 
