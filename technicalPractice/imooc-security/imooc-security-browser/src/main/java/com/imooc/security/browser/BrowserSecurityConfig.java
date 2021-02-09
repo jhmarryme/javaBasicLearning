d19b40084713d02740f2b76479605dd173fb25bf
wjh <wjh@travelsky.com>
2021-02-09 16:29:57 +0800

    oauth 学习

--- technicalPractice/imooc-security/imooc-security-browser/src/main/java/com/imooc/security/browser/BrowserSecurityConfig.java
+++ technicalPractice/imooc-security/imooc-security-browser/src/main/java/com/imooc/security/browser/BrowserSecurityConfig.java
@@ -60,16 +60,6 @@ public class BrowserSecurityConfig extends AbstractChannelSecurityConfig {
     private LogoutSuccessHandler logoutSuccessHandler;
 
     /**
-     * 对密码 使用加密处理, 使用spring security 提供的实现
-     * <br/>
-     * @return org.springframework.security.crypto.password.PasswordEncoder
-     */
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-
-    /**
      * rememberme 配置
      * <br/>
      * @author Jiahao Wang
