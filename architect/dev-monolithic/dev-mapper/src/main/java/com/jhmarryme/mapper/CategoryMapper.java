10b6aa086c51c499893d1f0ee9d8df68b1c6394f
jhmarryme <jhmarryme.cn@gmail.com>
2021-02-16 01:04:20 +0800

    mybatis逆向生成 文件引入

---
+++ architect/dev-monolithic/dev-mapper/src/main/java/com/jhmarryme/mapper/CategoryMapper.java
@@ -0,0 +1,7 @@
+package com.jhmarryme.mapper;
+
+import com.jhmarryme.my.mapper.MyMapper;
+import com.jhmarryme.pojo.Category;
+
+public interface CategoryMapper extends MyMapper<Category> {
+}
---
+++ architect/dev-monolithic/dev-mapper/src/main/java/com/jhmarryme/mapper/ItemsMapper.java
@@ -0,0 +1,7 @@
+package com.jhmarryme.mapper;
+
+import com.jhmarryme.my.mapper.MyMapper;
+import com.jhmarryme.pojo.Items;
+
+public interface ItemsMapper extends MyMapper<Items> {
+}
