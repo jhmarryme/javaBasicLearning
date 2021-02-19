7e109f2859022e73bf2a44e49401471113817585
wjh <wjh@travelsky.com>
2021-02-19 16:39:53 +0800

    oauth 学习

---
+++ technicalPractice/imooc-security/imooc-security-app/src/main/java/com/imooc/security/app/ImoocResourceServerConfig.java
@@ -0,0 +1,15 @@
+package com.imooc.security.app;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+
+/**
+ * description: 资源服务器
+ * @author Jiahao Wang
+ * @date 2021/2/19 10:43
+ */
+@Configuration
+@EnableResourceServer
+public class ImoocResourceServerConfig {
+
+}
