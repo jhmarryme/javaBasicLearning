6fc5a7d9884ca3de9c74b0ee56321cae62c6cb11
wjh <wjh@travelsky.com>
2021-02-24 17:43:33 +0800

    feat:
    基于简化模式返回的openid 进行登录

--- technicalPractice/imooc-security/imooc-security-core/src/main/java/com/imooc/security/core/properties/SecurityConstants.java
+++ technicalPractice/imooc-security/imooc-security-core/src/main/java/com/imooc/security/core/properties/SecurityConstants.java
@@ -30,6 +30,10 @@ public interface SecurityConstants {
 	 */
 	public static final String DEFAULT_LOGIN_PROCESSING_URL_MOBILE = "/authentication/mobile";
 	/**
+	 * 默认的基于openId的登录请求处理url
+	 */
+	public static final String DEFAULT_LOGIN_PROCESSING_URL_OPENID = "/authentication/openid";
+	/**
 	 * 默认登录页面
 	 * 
 	 * @see BrowserProperties
@@ -47,6 +51,14 @@ public interface SecurityConstants {
 	 * 发送短信验证码 或 验证短信验证码时，传递手机号的参数的名称
 	 */
 	public static final String DEFAULT_PARAMETER_NAME_MOBILE = "mobile";
+	/**
+	 * openId
+	 */
+	public static final String DEFAULT_PARAMETER_NAME_OPENID = "openId";
+	/**
+	 * providerId
+	 */
+	public static final String DEFAULT_PARAMETER_NAME_PROVIDERID = "providerId";
 	/**
 	 * session失效默认的跳转地址
 	 */
