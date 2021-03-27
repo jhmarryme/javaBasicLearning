4baf82f41cad2a0bc4247f1f1eca3dbee41a9b7b
jhmarryme <jhmarryme@gmail.com>
2021-03-27 23:05:14 +0800

    update

--- architect/dev-monolithic/dev-mapper/src/main/java/com/jhmarryme/mapper/ItemsMapperCustom.java
+++ architect/dev-monolithic/dev-mapper/src/main/java/com/jhmarryme/mapper/ItemsMapperCustom.java
@@ -1,6 +1,7 @@
 package com.jhmarryme.mapper;
 
 import com.jhmarryme.pojo.vo.ItemCommentVO;
+import com.jhmarryme.pojo.vo.SearchItemsVO;
 import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
@@ -11,12 +12,12 @@ public interface ItemsMapperCustom {
     List<ItemCommentVO> queryItemComments(@Param("paramsMap") Map<String, Object> map);
 
 
-   /* public List<SearchItemsVO> searchItems(@Param("paramsMap") Map<String, Object> map);
+    List<SearchItemsVO> searchItems(@Param("paramsMap") Map<String, Object> map);
 
-    public List<SearchItemsVO> searchItemsByThirdCat(@Param("paramsMap") Map<String, Object> map);
-
-    public List<ShopcartVO> queryItemsBySpecIds(@Param("paramsList") List specIdsList);
-
-    public int decreaseItemSpecStock(@Param("specId") String specId,
-                                     @Param("pendingCounts") int pendingCounts);*/
+//    public List<SearchItemsVO> searchItemsByThirdCat(@Param("paramsMap") Map<String, Object> map);
+//
+//    public List<ShopcartVO> queryItemsBySpecIds(@Param("paramsList") List specIdsList);
+//
+//    public int decreaseItemSpecStock(@Param("specId") String specId,
+//                                     @Param("pendingCounts") int pendingCounts);
 }
