47b0ef3a990bc0427a7c47dd325672c5b8748589
wjh <wjh@travelsky.com.cn>
2021-04-06 17:33:42 +0800

    部署到 云服务器 配置相关

--- architect/dev-monolithic-back/dev-api/src/main/java/com/jhmarryme/config/CorsConfig.java
+++ architect/dev-monolithic-back/dev-api/src/main/java/com/jhmarryme/config/CorsConfig.java
@@ -17,14 +17,14 @@ public class CorsConfig {
         // 1. 添加cors配置信息
         CorsConfiguration config = new CorsConfiguration();
         config.addAllowedOrigin("http://localhost:8080");
-        config.addAllowedOrigin("http://shop.z.mukewang.com:8080");
-        config.addAllowedOrigin("http://center.z.mukewang.com:8080");
-        config.addAllowedOrigin("http://shop.z.mukewang.com");
-        config.addAllowedOrigin("http://center.z.mukewang.com");
-        config.addAllowedOrigin("http://architect.center.jhmarryme.top:8080");
-        config.addAllowedOrigin("http://architect.shop.jhmarryme.top:8080");
-        config.addAllowedOrigin("http://architect.center.jhmarryme.top");
-        config.addAllowedOrigin("http://architect.shop.jhmarryme.top");
+//        config.addAllowedOrigin("http://shop.z.mukewang.com:8080");
+//        config.addAllowedOrigin("http://center.z.mukewang.com:8080");
+//        config.addAllowedOrigin("http://shop.z.mukewang.com");
+//        config.addAllowedOrigin("http://center.z.mukewang.com");
+        config.addAllowedOrigin("http://center.architect.jhmarryme.top:8080");
+        config.addAllowedOrigin("http://shop.architect.jhmarryme.top:8080");
+        config.addAllowedOrigin("http://center.architect.jhmarryme.top");
+        config.addAllowedOrigin("http://shop.architect.jhmarryme.top");
 
         // 设置是否发送cookie信息
         config.setAllowCredentials(true);
